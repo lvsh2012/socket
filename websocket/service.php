a6476da9e6c82021489b48d96a74f09c7108d487
kyx <kyx@example.com>
2016-09-20 15:35:51 +0800

    socket

--- websocket/service.php
+++ websocket/service.php
@@ -1,19 +1,5 @@
 <?php
 
-function getConnectionList($serv){
-    $start_fd = 0;
-    while(true)
-    {
-        $conn_list = $serv->connection_list($start_fd, 10);
-        if($conn_list===false or count($conn_list) === 0)
-        {
-            echo "finish\n";
-            break;
-        }
-        $start_fd = end($conn_list);
-        return $conn_list;
-    }
-}
 
 $serv = new swoole_websocket_server("192.168.146.128", 9502);
 
@@ -24,7 +10,19 @@ $serv->on('Open', function($server, $req) {
 $serv->on('Message', function($server, $frame) {
     echo "message: ".$frame->data."\r\n";
     $server->push($frame->fd, json_encode(["hello", "world"]));
-    $connectList = getConnectionList($serv);
+    $start_fd = 0;
+    while(true)
+    {
+        $connectList = $serv->connection_list($start_fd, 10);
+        if($connectList === false or count($connectList) === 0){
+            echo "finish\n";
+            break;
+        }else{
+
+			$start_fd = end($connectList);
+        }
+        
+    }
     foreach ($connectList as $fd){
         $server->push($fd, $frame->fd.":".$frame->data);
     }
