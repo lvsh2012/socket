be70b947f5207ac2dafd673d9458fc9ea98f1ca0
kyx <kyx@example.com>
2016-09-20 15:53:32 +0800

    socket

--- websocket/service.php
+++ websocket/service.php
@@ -5,26 +5,16 @@ $serv = new swoole_websocket_server("192.168.146.128", 9502);
 
 $serv->on('Open', function($server, $req) {
     echo "connection open: ".$req->fd."\r\n";
+    file_put_contents( __DIR__ .'/log.txt' , $request->fd);
 });
 
 $serv->on('Message', function($serv, $server, $frame) {
     echo "message: ".$frame->data."\r\n";
-    $server->push($frame->fd, json_encode(["hello", "world"]));
-    $start_fd = 0;
-    while(true)
-    {
-        $connectList = $serv->connection_list($start_fd, 10);
-        if($connectList === false or count($connectList) === 0){
-            echo "finish\n";
-            break;
-        }else{
-
-			$start_fd = end($connectList);
-        }
-        
-    }
-    foreach ($connectList as $fd){
-        $server->push($fd, $frame->fd.":".$frame->data);
+    $data = $frame->data;
+    $m = file_get_contents( __DIR__ .'/log.txt');
+    for ($i=1 ; $i<= $m ; $i++) {
+        echo PHP_EOL . '  i is  ' . $i .  '  data  is '.$data  . '  m = ' . $m;
+        $server->push($i, $data );
     }
 });
 
