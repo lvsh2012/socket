6c5c388262f8423a0986e5ec3989c60f7d537b8e
kyx <kyx@example.com>
2016-09-20 14:08:48 +0800

    socket

---
+++ websocket/clent.php
@@ -0,0 +1,37 @@
+<?php
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+//设置事件回调函数
+$client->on("connect", function($cli) {
+    $cli->send("hello world\r\n");
+});
+$client->on("receive", function($cli, $data){
+    echo "Received: ".$data."\r\n";
+});
+$client->on("error", function($cli){
+    echo "Connect failed\r\n";
+});
+$client->on("close", function($cli){
+    echo "Connection close\r\n";
+});
+//发起网络连接
+$client->connect('192.168.146.128', 9502, 0.5);
+
+function send($data, $type = 'text', $masked = false)
+{
+    switch($type)
+    {
+        case 'text':
+            $_type = WEBSOCKET_OPCODE_TEXT;
+            break;
+        case 'binary':
+        case 'bin':
+            $_type = WEBSOCKET_OPCODE_BINARY;
+            break;
+        default:
+            return false;
+    }
+    return $client ->send(swoole_websocket_server::pack($data, $_type, true, $masked));
+}
+
+$text = isset($_GET('text')) ? $_GET['text'] : time();
+send($text."\r\n");
--- websocket/service.php
+++ websocket/service.php
@@ -2,16 +2,36 @@
 $serv = new swoole_websocket_server("192.168.146.128", 9502);
 
 $serv->on('Open', function($server, $req) {
-    echo "connection open: ".$req->fd;
+    echo "connection open: ".$req->fd."\r\n";
 });
 
 $serv->on('Message', function($server, $frame) {
-    echo "message: ".$frame->data;
+    echo "message: ".$frame->data."\r\n";
     $server->push($frame->fd, json_encode(["hello", "world"]));
+    $connectList = getConnectionList($serv);
+    foreach ($connectList as $fd){
+        $server->push($fd, $frame->fd.":".$frame->data);
+    }
 });
 
 $serv->on('Close', function($server, $fd) {
-    echo "connection close: ".$fd;
+    echo "connection close: ".$fd."\r\n";
 });
 
 $serv->start();
+
+
+function getConnectionList($serv){
+    $start_fd = 0;
+    while(true)
+    {
+        $conn_list = $serv->connection_list($start_fd, 10);
+        if($conn_list===false or count($conn_list) === 0)
+        {
+            echo "finish\n";
+            break;
+        }
+        $start_fd = end($conn_list);
+        return $conn_list;
+    }
+}
